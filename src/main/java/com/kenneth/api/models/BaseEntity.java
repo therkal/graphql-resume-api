f05a2248c0e580039c13a45b1330252af05c43be
Kenneth Martens <kennethmartens@MacBook-13-Retina-2015.local>
2022-02-17 11:40:07 -0600

    Replaces Entity annotation with MongoEntity

--- src/main/java/com/kenneth/api/models/BaseEntity.java
+++ src/main/java/com/kenneth/api/models/BaseEntity.java
@@ -1,16 +1,23 @@
 package com.kenneth.api.models;
 
+import io.quarkus.mongodb.panache.common.MongoEntity;
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.bson.types.ObjectId;
 import org.eclipse.microprofile.graphql.Id;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
+@MongoEntity
 public class BaseEntity extends ReactivePanacheMongoEntity {
     @Id
     @GeneratedValue(generator="system-uuid")
     @GenericGenerator(name="system-uuid", strategy = "uuid")
     @Column(name = "uuid", unique = true)
-    private String id;
+    protected ObjectId id;
 }
